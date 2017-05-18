0035366d6a76f34054240702e0535518d1e9ac87
javister <javist.gr@mail.ru>
2017-05-19 02:06:48 +0500

    ciphering app
    cipher <input file> <key file>  -generate key for input file
    cipher <input file> <key file> <output file>   -cipher file with key

--- 1_OneTimePad/cipher/cipher/cipher.cpp
+++ 1_OneTimePad/cipher/cipher/cipher.cpp
@@ -6,6 +6,35 @@
 #include <locale>
 using namespace std;
 
+bool write(string &output, vector<unsigned char> mas)
+{
+	ofstream outfile(output, fstream::out | fstream::trunc | fstream::binary); //creating file
+	if (!outfile.is_open())return false;
+	for (int i = 0; i < mas.size(); i++)
+	{
+		outfile.put((unsigned char)mas.at(i));
+		//adding value to vector
+	}
+	outfile.close();
+	return true;
+} // writing random to file
+vector<unsigned char> full(string &source)
+{
+	ifstream _file(source, ios::in | ios::binary);
+	if (!_file.is_open()) { cout << "Wrong input file"; exit(1); }
+	_file.seekg(0, ios::end);
+	int size = _file.tellg();
+	_file.close();
+	//getting file size
+	vector<unsigned char> mas; //key vector
+	for (int i = 0; i < size; i++)
+	{
+		mas.push_back(rand() % 256);
+	} //filling key vector
+	return mas;
+} //random key generation
+// Functions for key generation
+
 class Foper // class for file works (read,write)
 {
 private:
@@ -74,18 +103,39 @@ bool code(string &inpath, string &keypath, string &outpath)
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "RUSSIAN");
-	if (!(argc == 4)) { cout << "Wrong arguments. cipher <source file> <key file> <result file>";  return 1; }
-	string input = argv[1];
-	string key = argv[2];
-	string output = argv[3];
-	if (input == output)
+	if (argc == 4)
 	{
-		cout << "The same file is used as source and result. Continue anyway? (y/n)";
-		char c;
-		cin >> c;
-		if (c == 'n')exit(2); else if (c != 'y')return 1;
+		string input = argv[1];
+		string key = argv[2];
+		string output = argv[3];
+
+		if (input == output)
+		{
+			cout << "The same file is used as source and result. Continue anyway? (y/n)";
+			char c;
+			cin >> c;
+			if (c == 'n')exit(2); else if (c != 'y')return 1;
+		}
+		if (!code(input, key, output)) { cout << "Error while ciphering."; return 1; };
+		cout << "Operation performed successfuly. Check file \"" << output << "\"";
 	}
-	if (!code(input, key, output)) { cout << "Error while ciphering."; return 1; };
-	cout << "Operation performed successfuly. Check file \"" << output << "\"";
+	else
+		if (argc == 3) {
+			string source = argv[1];
+			string keypath = argv[2];
+			if (source == keypath)
+			{
+				cout << "Are you sure you wish to replace source file? (y/n)";
+				char c;
+				cin >> c;
+				if (c == 'n')exit(2); else if (c != 'y')return 1;
+			}
+			cout << '"' << argv[1] << "\" should be ciphered with \"" << argv[2] << '"' << endl;
+			if (write(keypath, full(source)))cout << "Key file \"" + keypath + "\" created"; //writing key
+		}
+	else 
+	{
+	cout << "Wrong arguments. cipher <source file> <key file> <result file> for ciphering; cipher <source file> <key file> for key generation";  return 1;
+	}
 	return 0;
 }
