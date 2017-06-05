5dd89cd2909ffc69ce99ebe561f95659825425dc
javister <javist.gr@mail.ru>
2017-06-05 05:32:20 +0500

    rc4 cipher app

---
+++ 5_RC4/5_RC4/rc4.cpp
@@ -0,0 +1,86 @@
+#include "../../Foper.cpp"
+class rc4 {
+private:
+    const int blockLength = 8; //1byte block
+    int tableSize = 0;
+    vector<_byte> key;
+    vector<_byte> table;
+    _byte a = 0;
+    _byte b = 0;
+    void swap(int i, int j)
+    {
+        int tempval = table.at(j);
+        table.at(j) = table.at(i);
+        table.at(i) = tempval;
+    }
+    void ms()
+    {
+        for (int i = 0; i < pow(2, blockLength); i++)
+        {
+            table.push_back(i);
+        }
+        int j = 0;
+        int tempval = 0;
+        for (int i = 0; i < tableSize; i++)
+        {
+            j = (j + table.at(i) + key.at(i%key.size())) % 256;
+            swap(i, j);
+        }
+        for (int i = 0; i < tableSize; i++)std::cout << (int)table.at(i) << ' ';
+        system("pause");
+    }
+    _byte get()
+    {
+        a = (a + 1) % tableSize;
+        b = (b + table.at(b)) % tableSize;
+        swap(a, b);
+        _byte Ki = table.at((table.at(a) + table.at(b)) % tableSize);
+        return Ki;
+    }
+public:
+    rc4() {
+        tableSize = pow(2, blockLength);
+    };
+    void setKey(string &_key) { for (int i = 0; i < _key.size(); i++)key.push_back(_key[i]); };
+    void setKeyFromFile(string &path)
+    {
+        Foper keyfile;
+        if (!keyfile.open(path))
+        {
+            std::cout << "Key file not found";
+            return;
+        }
+        key = keyfile.GetData();
+    }
+    void createKey(int length) //file is required as copying from console is inconvenient
+    {
+        key.clear();
+        Foper keyfile;
+        for (int i = 0; i < length; i++)
+        {
+            _byte b = rand() % 256;
+            key.push_back(b);
+            std::cout << b;
+        }
+        keyfile.GetData() = key;
+        keyfile.write((string)"key.txt");
+    }
+    void code(string inputpath, string outputpath)
+    {
+        Foper input;
+        Foper output;
+        if (!input.open(inputpath))
+        {
+            std::cout << "Key file not found";
+            return;
+        }
+        a = 0;
+        b = 0;
+        table.clear();
+
+        ms();
+        for (int i = 0; i < input.GetData().size(); i++)
+            output.GetData().push_back(input.GetData().at(i) ^ get());
+        output.write(outputpath);
+    }
+};
---
+++ Foper.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#define _byte unsigned char
+#define string std::string
+#define vector std::vector
+#define ifstream std::ifstream
+class Foper
+{
+private:
+    vector<_byte> _data;
+public:
+    Foper()
+    {
+    }
+    bool open(string &name)
+    {
+        ifstream _file(name, std::ios::in | std::ios::binary);
+        if (!_file.is_open())
+        {
+            std::cout << "No such file: " << name << std::endl;
+            return false;
+        }
+
+        _file.seekg(0, std::ios::end);
+        int size = _file.tellg();
+        _file.seekg(0, std::ios::beg);
+
+        if (size != 0) {
+            _byte byte;
+            for (int i = 0; i < size; i++)
+            {
+                byte = (_byte)(_file.get());
+                _data.push_back(byte);
+            }
+        }
+        else {
+            std::cout << "Empty file";
+            _file.close();
+        }
+        return !_data.empty();
+    }
+    vector<_byte> &GetData() { return _data; }
+    bool write(string &output)
+    {
+        std::ofstream outfile(output, std::fstream::out | std::fstream::trunc | std::fstream::binary);
+        if (!outfile.is_open()) { std::cout << "Unable to open file for writing"; return false; };
+        for (int i = 0; i < _data.size(); i++)
+        {
+            outfile.put((_byte)_data.at(i));
+        }
+        outfile.close();
+        return true;
+    }
+};
