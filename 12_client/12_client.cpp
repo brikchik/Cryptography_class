8fe91c0285f0065d7be89a265e666d941bce29de
javister <javist.gr@mail.ru>
2017-07-02 14:10:23 +0500

    basic socket connection

--- 12_client/12_client.cpp
+++ 12_client/12_client.cpp
@@ -1,5 +1,95 @@
+#pragma once
+#include <time.h>
+#include <stdio.h>
+#include <winsock2.h>
+#include <iostream>
+#include "../THIRD-PARTY/src/mpir-3.0.0/lib/Win32/Debug/mpir.h"
+#pragma comment (lib, "../third-party/src/mpir-3.0.0/lib/Win32/Debug/mpir.lib")
+#pragma comment (lib, "Ws2_32.lib")
+//http://programmerinfo.ru/cplussocket/?v=f9308c5d0596
+int retVal = 0;
+SOCKET clientSock;
+int init()
+{
+    WORD ver = MAKEWORD(2, 2);
+    WSADATA wsaData;
+    WSAStartup(ver, (LPWSADATA)&wsaData);
+    LPHOSTENT hostEnt;
+    hostEnt = gethostbyname("localhost");
+    clientSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSock == SOCKET_ERROR)
+    {
+        printf("Unable to create socket\n");
+        WSACleanup();
+        return 1;
+    }
+    SOCKADDR_IN serverInfo;
+    serverInfo.sin_family = PF_INET;
+    serverInfo.sin_addr = *((LPIN_ADDR)*hostEnt->h_addr_list);
+    serverInfo.sin_port = htons(508);
+    retVal = connect(clientSock, (LPSOCKADDR)&serverInfo, sizeof(serverInfo));
+    if (retVal == SOCKET_ERROR)
+    {
+        printf("Unable to connect\n");
+        WSACleanup();
+        return 1;
+    }
+    printf("Connection made sucessfully\n");
+}
+void finish()
+{
+    closesocket(clientSock);
+    WSACleanup();
+}
+bool sendM(char* message, int size)
+{
+    retVal = send(clientSock, message, size, 0);
+    if (retVal == SOCKET_ERROR)
+    {
+        printf("Unable to send\n");
+        WSACleanup();
+        return false;
+    }
+    else
+        return true;
+}
+char* getM(int size)
+{
+    char* message=new char[size];
+    retVal = recv(clientSock, message, size, 0);
+    if (retVal == SOCKET_ERROR)
+    {
+        printf("Unable to recv\n");
+        WSACleanup();
+        return message;
+    }
+    else
+        return message;
+}
+bool Diffie_hellman()
+{
+    mpz_t A;
+    mpz_init(A);
+    gmp_randstate_t r_state;
+    gmp_randinit_default(r_state);
+    gmp_randseed_ui(r_state, time(NULL));
+    mpz_init(A);
+    mpz_urandomb(A, r_state, 80);
+    gmp_printf("My number: %Zd\n", A);
+    gmp_randclear(r_state);
+    mpz_clear(A);
+    init();
+    printf("Sending request from client\n");
+    char pBuf[1] = { 'k' };
+    sendM(pBuf, 25);
+    char* szResponse = getM(25);
+    printf("Got the response from server\n%s\n", szResponse);
+    finish();
+    return true;
+}
 int main()
 {
+    if (!Diffie_hellman())return 1;
+    system("pause");
     return 0;
 }
-
--- 12_server/12_server.cpp
+++ 12_server/12_server.cpp
@@ -1,5 +1,101 @@
-int main()
+#pragma comment (lib, "Ws2_32.lib")
+#include <time.h>
+#include "../THIRD-PARTY/src/mpir-3.0.0/lib/Win32/Debug/mpir.h"
+#pragma comment (lib, "../third-party/src/mpir-3.0.0/lib/Win32/Debug/mpir.lib")
+#include <winsock2.h>
+#include <stdio.h>
+//http://programmerinfo.ru/cplussocket/?v=f9308c5d0596
+int retVal=0;
+SOCKET servSock;
+SOCKET clientSock;
+WSADATA wsaData;
+bool sendM(char* message, int size)
 {
+    retVal = send(clientSock, message, size, 0);
+    if (retVal == SOCKET_ERROR)
+    {
+        printf("Unable to send\n");
+        WSACleanup();
+        return false;
+    }
+    else
+        return true;
+}
+char* getM(int size)
+{
+    char* message = new char[size];
+    retVal = recv(clientSock, message, size, 0);
+    if (retVal == SOCKET_ERROR)
+    {
+        printf("Unable to recv\n");
+        WSACleanup();
+    }
+    return message;
+}
+void init()
+{
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (servSock == INVALID_SOCKET)
+    {
+        printf("Unable to create socket\n");
+        WSACleanup();
+        return;
+    }
+    SOCKADDR_IN sin;
+    sin.sin_family = PF_INET;
+    sin.sin_port = htons(508);
+    sin.sin_addr.s_addr = INADDR_ANY;
+
+    retVal = bind(servSock, (LPSOCKADDR)&sin, sizeof(sin));
+    if (retVal == SOCKET_ERROR)
+    {
+        printf("Unable to bind\n");
+        WSACleanup();
+        return;
+    }
+    retVal = listen(servSock, 10);
+    if (retVal == SOCKET_ERROR)
+    {
+        printf("Unable to listen\n");
+        WSACleanup();
+        return;
+    }
+    clientSock = accept(servSock, NULL, NULL);
+    if (clientSock == INVALID_SOCKET)
+    {
+        printf("Unable to accept\n");
+        WSACleanup();
+        return ;
+    }
+}
+void finish()
+{
+    closesocket(clientSock);
+    closesocket(servSock);
+    WSACleanup();
+}
+bool Diffie_hellman()
+{
+    mpz_t B;
+    mpz_init(B);
+    gmp_randstate_t r_state;
+    gmp_randinit_default(r_state);
+    gmp_randseed_ui(r_state, time(NULL));
+    mpz_init(B);
+    mpz_urandomb(B, r_state, 80);
+    gmp_printf("My number: %Zd\n", B);
+    gmp_randclear(r_state);
+    mpz_clear(B);
+    init();
+    printf("Got the request from client\n%s\n", getM(25));
+    char *szResp = "Response";
+    if (sendM(szResp, 25))printf("Sent response from server\n");
+    finish();
+    return true;
+}
+int main(void)
+{
+    if (!Diffie_hellman())return 1;
     return 0;
 }
-
